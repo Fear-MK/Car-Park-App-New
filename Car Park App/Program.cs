cb1eeecc7e1ef78d57995e3faf2374da1ae1258c
AQ233406 <AQ233406@F31_10>
2023-09-22 10:28:44 +0100

    Add project files.

---
+++ Car Park App/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections;
+using static cogs;
+
+
+namespace Car_Park_App
+{
+    internal class Program
+    {
+        static void Reset() //This is just a class to make the rest of the code a little more clear
+        {
+            Console.Clear();
+            Console.WriteLine("####################################################\n\n  W E L C O M E   T O   B O B ' S   C A R   P A R K\n\n####################################################");
+        }
+
+        static void Main()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black; // It is inefficient to add this to Reset
+            Reset();
+
+            Thread.Sleep(500); //Pausescode execution for 1000 milliseconds
+            string reg_plate;
+            while (true) // Simple Validation
+            {
+                Console.WriteLine("\nPlease enter the car registration: ");
+                reg_plate = Console.ReadLine().ToUpper();
+                Console.WriteLine("Registration entered: " +reg_plate+" \n\nIs this correct? (y/n)");
+                try
+                {
+                    char reg_plate_confimation = char.Parse(Console.ReadLine());
+                    if (reg_plate_confimation == 'y')
+                    {
+                        break;
+                    }
+                    Thread.Sleep(1000);
+                }
+                catch
+                {
+                    Console.WriteLine("\nInvalid data entered.");
+                    Thread.Sleep(1000);
+                }
+            }
+            Reset(); //Clears screen ready for the parking options
+
+            (Dictionary<int, int>, string[], Dictionary<int, float>) menu_tuple = cogs.DisplayMenu();
+            if (menu_tuple == (null, null, null))
+            {
+                Console.WriteLine("There was an error with the menu properties file, contact an administrator immediately.");
+            }
+            Dictionary <int, int> dictionary = menu_tuple.Item1;
+            string[] times = menu_tuple.Item2;
+            Dictionary<int, float> timesDict = menu_tuple.Item3;
+
+            int choice = 0;
+
+            while (true)
+            {
+                try
+                {
+                    choice = int.Parse(Console.ReadLine());
+                    if (choice < 1 || choice > dictionary.Count)
+                    {
+                        Console.WriteLine("Invalid entry, please enter a valid option (1-" + dictionary.Count + ")");
+                    }
+                    else
+                    {
+                        Reset();
+                        Console.WriteLine("{0} selected. ", times[choice-1]);
+                        break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid entry, please enter a valid option (1-" + dictionary.Count + ")");
+                }
+            }
+
+            float price = dictionary[choice]; float length_of_ticket = timesDict[choice]; float due = price;
+            float paid = 0; float total_paid = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Amount Due: {0:C2}\nPlease enter insert coins below.", due);
+                try
+                {
+                    paid = float.Parse(Console.ReadLine()); //this is for this round of the loop
+                    total_paid += paid;
+
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid coin amount entered (symbol not necessary, please try again. (Press any key to retry)");
+                    Console.ReadKey();
+                    Reset(); continue;
+                }
+
+                Console.WriteLine("Amount Entered: {0:C2}", price);
+                if (price - total_paid <= 0)
+                {
+                    Console.WriteLine("Amount inserted in total: {0:C2}", total_paid);
+                    Thread.Sleep(2000);
+                    Console.WriteLine("Change needed: {0:C2}", (price - total_paid) * -1);
+                    Thread.Sleep(2000);
+                    cogs.PrintTicket(reg_plate, price, total_paid, length_of_ticket);
+                    Main();
+                }
+                else
+                {
+                    due -= paid;
+                    Reset();
+                }
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Car Park App/cogs.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections;
+
+public class cogs
+{
+    internal static (Dictionary<int, int>, string[], Dictionary<int, float>) DisplayMenu()
+    {
+        Dictionary<int, int> dictionary = new Dictionary<int, int>(); //This upcoming section is making the dictionary from the menu.txt, so that any modifications made in the "management mode" will be saved for when the program loads up again.
+        List<string> lines = null;
+
+        try
+        {
+            lines = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "menu.txt")).ToList();
+        }
+        catch
+        {
+            Console.WriteLine("Could not find menu.txt, please create one.");
+            Environment.Exit(400);
+        }
+
+        Dictionary<int, float> timesDict = new Dictionary<int, float>();
+        List<string> times = new List<string>();    
+        int count1 = 0;
+
+        foreach (string line in lines ?? new List<string>())
+        {
+            count1++;
+            string[] parts = line.Split('=');
+            try
+            {
+                times.Add(parts[0]);
+                dictionary.Add(count1, int.Parse(parts[1]));
+                timesDict.Add(count1, float.Parse(parts[2]));
+            }
+            catch
+            {
+                return (null, null, null);
+            }
+
+            string menu_before_spaces = string.Concat("{0}      {1}£{2}", count1.ToString(), parts[0], parts[1]);
+            int Length = string.Concat("{0}      {1}£{2}", count1.ToString(), parts[0], parts[1]).Length;
+
+            int number_of_spaces = 40 - Length;
+            string spaces = "";
+            for (int i = 0; i < number_of_spaces; i++)
+            {
+                spaces += " ";
+            }
+
+            Console.WriteLine("{0}      {1}{2}£{3}", count1, parts[0], spaces, parts[1]);
+        }
+
+
+        Console.WriteLine("\nPlease select duration:       (Press number 1-" + dictionary.Count + ")\n");
+        return (dictionary, times.ToArray(), timesDict);
+    }
+    internal static void PrintTicket(string registration, float fee, float change, float time) //needs expiry and date entered etc
+    {
+        Console.Clear();
+        Console.Write("Printing ticket");
+        for (int i = 0;i <= 3;i++)
+        {
+            Thread.Sleep(400);
+            Console.Write(". ");
+        }
+        Console.Write("\n");
+        Console.BackgroundColor = ConsoleColor.White;
+        string[] ticket_text = TicketTextGen(registration, fee, change, time);
+        int longest_line = 0;
+        foreach (string line in ticket_text)
+        {
+            if (line.Length > longest_line)
+            {
+                longest_line = line.Length;
+            }
+        }
+        foreach (string line in ticket_text)
+        {
+            Thread.Sleep(200);
+            string modifiedLine = line;
+            while (modifiedLine.Length < longest_line)
+            {
+                modifiedLine += " ";
+            }
+            Console.WriteLine(modifiedLine);
+        }
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Press any key to exit..."); Console.ReadKey();
+
+    }
+
+    internal static string[] TicketTextGen(string registration, float fee, float change, float time)
+    {
+        DateTime timenow = DateTime.Now;
+        DateTime expirytime = timenow.AddHours(time);
+        string text = "                             \n  B O B ' S  C A R  P A R K  \n\n\nRegistration: " + registration + "\n\nEntry Time: " + DateTime.Now + "\n\nFee: " + fee.ToString("C2") + "\n\nValid Until: " + expirytime + "\n\nHave a nice day!"; return text.Split('\n');
+    }
+
+    internal static void saveTicket(string registration, float fee, float time)
+    {
+        StreamWriter sw;
+        if (!File.Exists("ticket_log.txt"))
+        {
+            sw = File.CreateText("ticket_log.txt");
+        }
+        else
+        {
+            sw = File.AppendText("ticket_log.txt");
+
+        }
+        sw.WriteLine("This is the content of the new file.");
+        sw.Close()
+    }
+}
